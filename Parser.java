35f5175409cd34593934f2f4320434aba49a70c0
reimu <r2hkri@gmail.com>
2017-04-28 13:39:16 -0500

    initial commit

---
+++ Parser.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+
+public class Parser {
+    public static void main(String[] args) {
+		
+        try {
+            PackageScanner scanner = new PackageScanner(args[0]);
+            // System.out.println(scanner.loaded());
+            String s;
+            System.out.println(scanner);
+        } catch (Exception e) {
+            System.out.println(e);
+			e.printStackTrace();
+        }
+    }
+}
